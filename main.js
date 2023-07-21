84a222f55b04b1df3be5d73c923af5b9d43d54db
Mukund Solanki <mukundsolanki665@gmail.com>
2023-07-21 23:29:04 +0530

    Fronted UI part completed

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const form = document.querySelector('form');
 
-setupCounter(document.querySelector('#counter'))
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+
+  const response = await fetch('http://localhost:8080/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt'),
+    }),
+  });
+
+  if (response.ok) {
+    const { image } = await response.json();
+
+    const result = document.querySelector('#result');
+    result.innerHTML = `<img src="${image}" width="512" />`;
+  } else {
+    const err = await response.text();
+    alert(err);
+    console.error(err);
+  }
+});
